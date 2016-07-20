bdc0798f8b8d7e03e77ea253a97c7d11b242f6ed
Aditya Ramesh <_@adityaramesh.com>
2016-07-20 17:42:16 -0400

    Comments to clarify code.

--- source/preprocess_birds.py
+++ source/preprocess_birds.py
@@ -1,7 +1,7 @@
 from birds_common import *
 
 dst_size = 64
-dst_img = Image.new(mode='RGB', size=(dst_size, dst_size))
+dst_img = Image.new(mode='RGB', size=(dst_size, dst_size), color='black')
 dst_draw = Image.draw(dst_img)
 
 total_count = 0
@@ -10,8 +10,15 @@ skipped_count = 0
 def process_img(img, bb):
     x, y, w, h = bb
     img_width, img_height = img.size
-    img_count = img_count + 1
+    total_count = total_count + 1
 
+    """
+    We skip over images for which it is not possible to obtain a square region containing the
+    bounding box that can be resized to `dst_size`. The fraction of such images is 7990 / 48562
+    (about 16.45% of the dataset).
+
+    TODO: cropping with spatial reflection padding?
+    """
     extra_space = 0
 
     if w != h:
@@ -24,6 +31,9 @@ def process_img(img, bb):
             if h + extra_space < w:
                 return
 
-    dst_draw.rectangle(xy=[(0, 0), dst_img.size], fill='black')
+    # TODO: compute crop parameters
+
+    # Should not be necessary.
+    # dst_draw.rectangle(xy=[(0, 0), dst_img.size], fill='black')
 
 for_each_img_and_bb(process_img, save=True)
