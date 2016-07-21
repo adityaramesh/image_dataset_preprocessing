63a4f47ee4293247f71868f545fed312e50e069f
Aditya Ramesh <_@adityaramesh.com>
2016-07-20 22:48:53 -0400

    Finished code to preprocess birds.

--- source/preprocess_birds.py
+++ source/preprocess_birds.py
@@ -1,39 +1,86 @@
+import math
 from birds_common import *
 
 dst_size = 64
-dst_img = Image.new(mode='RGB', size=(dst_size, dst_size), color='black')
-dst_draw = Image.draw(dst_img)
-
 total_count = 0
 skipped_count = 0
 
 def process_img(img, bb):
+    global total_count, skipped_count
+
     x, y, w, h = bb
     img_width, img_height = img.size
     total_count = total_count + 1
 
+    assert x >= 0
+    assert y >= 0
+
+    # These assertions can fail, so we need to threshold the width and height of the bounding box.
+    # assert x + w - 1 <= img_width
+    # assert y + h - 1 <= img_height
+    
+    w = min(w, img_width - x - 1)
+    h = min(h, img_height - y - 1)
+
     """
     We skip over images for which it is not possible to obtain a square region containing the
-    bounding box that can be resized to `dst_size`. The fraction of such images is 7990 / 48562
+    bounding box that can be resized to `dst_size`. The fraction of such images is 7988 / 48562
     (about 16.45% of the dataset).
 
     TODO: cropping with spatial reflection padding?
     """
-    extra_space = 0
+    x1, y1, x2, y2 = None, None, None, None
 
     if w != h:
         if w < h:
-            extra_space = img_width - w
-            if w + extra_space < h:
+            if img_width < h:
+                skipped_count = skipped_count + 1
                 return
+
+            extra_width = h - w
+            y1, y2 = y, y + h - 1
+
+            # We need to determine the left and right endpoints of the region containing the
+            # bounding box, so that the region's width comes out to `h`.
+            if x <= (img_width - 1) - (x + w - 1):
+                # There is more space to the right of the bounding box than to the left. Try to take
+                # up to `floor(h / 2)` pixels from the left, and all the rest from the right.
+                x1 = max(0, x - math.floor(extra_width / 2))
+                x2 = (x + w - 1) + (extra_width - (x - x1))
+                assert x2 <= img_width - 1
+                assert x2 - x1 + 1 == h
+            else:
+                # There is more space to the left of the bounding box than to the right. Try to take
+                # up to `floor(h / 2)` pixels from the right, and all the rest from the left.
+                x2 = min(img_width - 1, (x + w - 1) + math.floor(extra_width / 2))
+                x1 = x - (extra_width - (x2 - (x + w - 1)))
+                assert x1 >= 0
+                assert x2 - x1 + 1 == h
         else:
-            extra_space = img_height - h
-            if h + extra_space < w:
+            if img_height < w:
+                skipped_count = skipped_count + 1
                 return
 
-    # TODO: compute crop parameters
+            extra_width = w - h
+            x1, x2 = x, x + w - 1
 
-    # Should not be necessary.
-    # dst_draw.rectangle(xy=[(0, 0), dst_img.size], fill='black')
+            if y <= (img_height - 1) - (y + h - 1):
+                y1 = max(0, y - math.floor(extra_width / 2))
+                y2 = (y + h - 1) + (extra_width - (y - y1))
+                assert y2 <= img_height - 1
+                assert y2 - y1 + 1 == w
+            else:
+                y2 = min(img_height - 1, (y + h - 1) + math.floor(extra_width / 2))
+                y1 = y - (extra_width - (y2 - (y + h - 1)))
+                assert y1 >= 0
+                assert y2 - y1 + 1 == w
+    else:
+        x1, y1, x2, y2 = x, y, x + w - 1, y + h - 1
 
-for_each_img_and_bb(process_img, save=True)
+    img = img.crop(box=(x1, y1, x2, y2))
+    img = img.resize(size=(dst_size, dst_size), resample=Image.LANCZOS)
+
+for_each_img_and_bb(process_img, save=False)
+
+print("Fraction of images skipepd: {} / {} ({}%).".format(skipped_count, total_count,
+    100 * skipped_count / total_count))
--- source/render_bb_birds.py
+++ source/render_bb_birds.py
@@ -1,34 +1,38 @@
 from birds_common import *
 
-img_count = 0
-border_count = 0
+total_count = 0
+skipped_count = 0
+small_count = 0
 
 def process_img(img, bb):
-    global img_count, border_count
+    global total_count, skipped_count, small_count
 
     x, y, w, h = bb
     img_width, img_height = img.size
-    img_count = img_count + 1
+    total_count = total_count + 1
 
     if w != h:
         if w < h:
-            extra_space = img_width - w
-            if w + extra_space < h:
-                border_count = border_count + 1
+            if img_width < h:
+                skipped_count = skipped_count + 1
         else:
-            extra_space = img_height - h
-            if h + extra_space < w:
-                border_count = border_count + 1
+            if img_height < w:
+                skipped_count = skipped_count + 1
+
+    if max(w, h) < 64:
+        small_count = small_count + 1
 
     """
     # Draws the bounding box on the image.
 
     draw = ImageDraw.Draw(img)
-    draw.rectangle(xy=[(x, y), (x + w, y + h)], fill=None, outline='green')
+    draw.rectangle(xy=[(x, y), (x + w - 1, y + h - 1)], fill=None, outline='green')
     del draw
     """
 
 for_each_img_and_bb(process_img, save=False)
 
-print("Images requiring borders after cropping: {} / {} ({}%).".format(border_count, img_count,
-    100 * border_count / img_count))
+print("Images requiring borders after cropping: {} / {} ({}%).".format(skipped_count, total_count,
+    100 * skipped_count / total_count))
+
+print("Bounding boxes with largest dimension less than 64: {}.".format(small_count))
