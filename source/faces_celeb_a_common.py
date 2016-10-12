cf97b1b48c28571b1e169e16d6f1406882c02811
Aditya Ramesh <_@adityaramesh.com>
2016-10-12 01:20:53 -0400

    Code to preprocess Celeb A faces.

---
+++ source/faces_celeb_a_common.py
@@ -0,0 +1,27 @@
+import os
+from PIL import Image
+
+def for_each_img(func):
+    input_img_dir = '/home/aditya/data/celeb_a/img_align_celeba'
+    output_img_dir = 'output/celeb_a_cropped'
+
+    assert not os.path.exists(output_img_dir)
+    os.mkdir(output_img_dir)
+
+    counter = 0
+
+    for img_name in os.listdir(input_img_dir):
+        if img_name[0] == '.' or img_name[-1] == '~':
+            continue
+
+        counter = counter + 1
+        print("Working on image {}.".format(counter))
+
+        input_path  = os.path.join(input_img_dir, img_name)
+        output_path = os.path.join(output_img_dir, img_name)
+
+        img = Image.open(input_path)
+        new_img = func(img)
+
+        if new_img:
+            new_img.save(output_path, 'jpeg')
---
+++ source/preprocess_faces_celeb_a.py
@@ -0,0 +1,26 @@
+import math
+from PIL import Image, ImageDraw
+from faces_celeb_a_common import for_each_img
+
+# All images in the aligned CelebA dataset have the same dimensions.
+src_width  = 178
+src_height = 218
+
+final_size = 64
+dst_height = math.ceil(src_height * final_size / src_width)
+
+crop_right  = final_size - 1
+extra_space = dst_height - final_size
+crop_top    = math.floor(extra_space / 2)
+crop_bottom = crop_top + final_size - 1
+
+assert crop_bottom - crop_top == final_size - 1
+
+dst_img  = Image.new(size=(final_size, final_size), mode='RGB')
+dst_draw = ImageDraw.Draw(dst_img)
+
+def process_img(img):
+    img = img.resize(size=(final_size, dst_height), resample=Image.LANCZOS)
+    return img.crop(box=(0, crop_top, crop_right, crop_bottom))
+
+for_each_img(process_img)
